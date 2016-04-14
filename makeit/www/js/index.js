eb012566eab9c86853f1576160b320d93a2acbef
Zizitopiette <bachelor3@Mac-c42c032b9bbe.fro-207.froment.in.iesanetwork.com>
2016-04-14 12:59:37 +0200

    Add new feature photo to add a profil picture

--- makeit/www/js/index.js
+++ makeit/www/js/index.js
@@ -20,8 +20,74 @@ var app = {
    // function, we must explicitly call 'app.receivedEvent(...);'
    onDeviceReady: function () {
       app.receivedEvent('deviceready');
+
+      //Call function for profil picture
+      app.profilPicture();
+
    },
 
+   //Starter photo
+   //-----------------------------------
+
+   profilPicture: function () {
+
+      var pictureSource;
+      pictureSource = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+
+      $('#ProfilPicture').click(function() {
+         var $self = $(this);
+         
+         $(".picture-btn").removeClass('hide-btn');
+         $self.addClass('hide-btn');
+      })
+
+      $('#CameraShot').click(function() {
+         app.takePicture();
+      });
+      $('#CameraAlbum').click(function() {
+         app.getPhoto(pictureSource);
+      });
+
+   },
+
+   takePicture: function() {
+    // Take picture using device camera and retrieve image as base64-encoded string
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+        function onSuccess(imageURI) {
+            var image = $('#Image')[0];
+            image.src = imageURI;
+        };
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        };
+    },
+
+    getPhoto: function(source) {
+    //document.getElementById('timestamp').innerHTML = new Date();
+        // Retrieve image file location from specified source
+        navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+            quality: 50,
+            targetWidth: 500,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: source
+        });
+
+        function onPhotoURISuccess(imageURI) {
+        
+            var galleryImage = $('#Image')[0];
+            galleryImage.style.display = 'block';
+            galleryImage.src = imageURI;
+        };
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        };
+    },
+   //-----------------------------------
+   // END starter photo
 
    // Update DOM on a Received Event
    receivedEvent: function (id) {
