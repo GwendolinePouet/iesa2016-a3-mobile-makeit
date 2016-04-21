6f12c4f1094f21103510154701f007c11e15ef0e
Erwin Schwartz <erwin.schwartz.etudiant@gmail.com>
2016-04-21 16:33:37 +0200

    Mise en place du framework + plugin status bar + plugin networkinformation

--- makeit/platforms/ios/www/cordova_plugins.js
+++ makeit/platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,55 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
         "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
         "id": "cordova-plugin-camera.Camera",
         "pluginId": "cordova-plugin-camera",
@@ -33,6 +82,54 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+        "id": "cordova-plugin-globalization.GlobalizationError",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "window.GlobalizationError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+        "id": "cordova-plugin-globalization.globalization",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "navigator.globalization"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
+        "id": "cordova-plugin-geolocation.Coordinates",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Position.js",
+        "id": "cordova-plugin-geolocation.Position",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-contacts/www/contacts.js",
         "id": "cordova-plugin-contacts.contacts",
         "pluginId": "cordova-plugin-contacts",
@@ -47,6 +144,11 @@ module.exports = [
         "clobbers": [
             "Contact"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/convertUtils.js",
+        "id": "cordova-plugin-contacts.convertUtils",
+        "pluginId": "cordova-plugin-contacts"
     },
     {
         "file": "plugins/cordova-plugin-contacts/www/ContactAddress.js",
@@ -121,62 +223,38 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
-        "id": "cordova-plugin-geolocation.Coordinates",
-        "pluginId": "cordova-plugin-geolocation",
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
         "clobbers": [
-            "Coordinates"
+            "navigator.connection",
+            "navigator.network.connection"
         ]
     },
     {
-        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
-        "id": "cordova-plugin-geolocation.PositionError",
-        "pluginId": "cordova-plugin-geolocation",
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
         "clobbers": [
-            "PositionError"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-geolocation/www/Position.js",
-        "id": "cordova-plugin-geolocation.Position",
-        "pluginId": "cordova-plugin-geolocation",
-        "clobbers": [
-            "Position"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
-        "id": "cordova-plugin-geolocation.geolocation",
-        "pluginId": "cordova-plugin-geolocation",
-        "clobbers": [
-            "navigator.geolocation"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
-        "id": "cordova-plugin-globalization.GlobalizationError",
-        "pluginId": "cordova-plugin-globalization",
-        "clobbers": [
-            "window.GlobalizationError"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
-        "id": "cordova-plugin-globalization.globalization",
-        "pluginId": "cordova-plugin-globalization",
-        "clobbers": [
-            "navigator.globalization"
+            "Connection"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-camera": "2.1.1",
-    "cordova-plugin-contacts": "2.0.1",
-    "cordova-plugin-geolocation": "2.1.0",
+    "cordova-plugin-console": "1.0.3",
+    "cordova-plugin-device": "1.1.2",
+    "cordova-plugin-splashscreen": "3.2.2",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-whitelist": "1.2.2",
+    "ionic-plugin-keyboard": "2.0.1",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-camera": "2.2.0",
     "cordova-plugin-globalization": "1.0.3",
-    "cordova-plugin-whitelist": "1.2.1"
+    "cordova-plugin-geolocation": "2.2.0",
+    "cordova-plugin-contacts": "2.1.0",
+    "cordova-plugin-network-information": "1.2.1"
 }
 // BOTTOM OF METADATA
 });
---
+++ makeit/www/js/script.js
@@ -0,0 +1,257 @@
+var app = {
+   // Application Constructor
+   initialize: function () {
+      this.bindEvents();
+   },
+
+
+   // Bind Event Listeners
+   //
+   // Bind any events that are required on startup. Common events are:
+   // 'load', 'deviceready', 'offline', and 'online'.
+   bindEvents: function () {
+      document.addEventListener('deviceready', this.onDeviceReady, false);
+   },
+
+
+   // deviceready Event Handler
+   //
+   // The scope of 'this' is the event. In order to call the 'receivedEvent'
+   // function, we must explicitly call 'app.receivedEvent(...);'
+   onDeviceReady: function () {
+      StatusBar.hide();
+
+      document.addEventListener("offline", app.checkConnection, false);
+      document.addEventListener("online", app.checkConnection, false);
+
+
+      app.receivedEvent('deviceready');
+
+      //Call function for profil picture
+      app.profilPicture();
+      $('#contact-btn').click(function () {
+         app.findContacts();
+      });
+
+      //Call Geolocation feature
+      $('#geolocation-btn').click(function () {
+         app.geolocationTuto();
+      });
+
+      app.langSelect();
+   },
+
+   //Starter photo
+   //-----------------------------------
+
+   profilPicture: function () {
+
+      var pictureSource;
+      pictureSource = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+
+      $('#ProfilPicture').click(function () {
+         var $self = $(this);
+
+         $(".picture-btn").removeClass('hide-btn');
+         $self.addClass('hide-btn');
+      })
+
+      $('#CameraShot').click(function () {
+         app.takePicture();
+      });
+      $('#CameraAlbum').click(function () {
+         app.getPhoto(pictureSource);
+      });
+
+   },
+
+   // GLOBALIZATION ---------------------------------------------------
+
+   langSelect: function () {
+      navigator.globalization.getPreferredLanguage(
+         function (language) {
+            var lang = language.value;
+
+            var $checkLang = $('.checkLang');
+
+            if (lang == 'fr-US' || lang == 'fr-FR') {
+               $checkLang.html('V&eacute;rifier la langue');
+               $('#contact-btn a').html('Trouver des amis');
+            } else {
+               $checkLang.html('Check language');
+               $('#contact-btn a').html('Find friends');
+            }
+
+            $checkLang.on('click', function () {
+               alert(lang);
+            });
+
+         },
+         function () {
+            alert('Error getting language\n');
+         }
+      );
+   },
+
+   // END GLOBALIZATION
+
+   takePicture: function () {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onSuccess, onFail, {
+         quality: 50,
+         destinationType: Camera.DestinationType.FILE_URI
+      });
+
+      function onSuccess(imageURI) {
+         var image = $('#Image')[0];
+         image.src = imageURI;
+      };
+
+      function onFail(message) {
+         alert('Failed because: ' + message);
+      };
+   },
+
+   getPhoto: function (source) {
+      //document.getElementById('timestamp').innerHTML = new Date();
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+         quality: 50,
+         targetWidth: 500,
+         destinationType: Camera.DestinationType.FILE_URI,
+         sourceType: source
+      });
+
+      function onPhotoURISuccess(imageURI) {
+
+         var galleryImage = $('#Image')[0];
+         galleryImage.style.display = 'block';
+         galleryImage.src = imageURI;
+      };
+
+      function onFail(message) {
+         alert('Failed because: ' + message);
+      };
+   },
+   //-----------------------------------
+   // END starter photo
+
+   //STARTER NETWORK INFORMATION
+   //--------------------------------------------
+
+   checkConnection: function () {
+
+
+      var networkState = navigator.connection.type;
+      var states = {};
+      states[Connection.UNKNOWN] = 'Unknown connection';
+      states[Connection.ETHERNET] = 'Ethernet connection';
+      states[Connection.WIFI] = 'WiFi connection';
+      states[Connection.CELL_2G] = 'Cell 2G connection';
+      states[Connection.CELL_3G] = 'Cell 3G connection';
+      states[Connection.CELL_4G] = 'Cell 4G connection';
+      states[Connection.CELL] = 'Cell generic connection';
+      states[Connection.NONE] = 'No network connection';
+      alert('Connection type: ' + states[networkState]);
+
+   },
+   //END NETWORK INFORMATION---------------------------------
+
+   //Starter Geolocation
+   //---------------------------
+   geolocationTuto: function () {
+
+
+      navigator.geolocation.getCurrentPosition(geolocationSuccess, onGeolocError);
+
+
+      //onSuccess Geolocation
+
+      function geolocationSuccess(position) {
+         var element = document.getElementById('geolocation');
+         element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+            'Longitude: ' + position.coords.longitude + '<br />' +
+            'Altitude: ' + position.coords.altitude + '<br />' +
+            'Accuracy: ' + position.coords.accuracy + '<br />' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
+            'Heading: ' + position.coords.heading + '<br />' +
+            'Speed: ' + position.coords.speed + '<br />' +
+            'Timestamp: ' + position.timestamp + '<br />';
+      };
+
+      // onError Callback receives a PositionError object
+      //
+      function onGeolocError(error) {
+         alert('code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
+      };
+   },
+   //---------------------------
+   //End Starter Geolocation
+   //Starter Contacts
+   findContacts: function () {
+
+      var listNumber;
+      $.ajax({
+         url: 'js/base.json',
+         success: function (data) {
+            //it works, do something with the data
+            var donnees = $.parseJSON(data);
+            listNumber = donnees.numeros;
+            displayContacts();
+         },
+         error: function () {
+            //something went wrong, handle the error and display a message
+            $('.titrelistcontacts').text("error");
+         }
+      });
+
+      var displayContacts = function () {
+         $('.titrelistcontacts').text("Liste de vos contacts utilisant l'application");
+         //lecture de tout le tab
+         for (var i = 0; i < listNumber.length; i++) {
+            var finder = listNumber[i];
+
+            function onSuccess(contacts) {
+               var text = '';
+
+               if (!!contacts[0].name.givenName) {
+                  text = contacts[0].name.givenName;
+               }
+
+               if (!!contacts[0].name.familyName) {
+                  text = text + ' ' + contacts[0].name.familyName;
+               }
+
+               $('#list').append("<li>" + text + ' | ' + contacts[0].phoneNumbers[0].value + "</li>");
+               $('.nombrelistcontacts').text($('#list li').length + " de vos contacts utilisent l'application.");
+            };
+
+            function onError(contactError) {
+               alert('onError!');
+            };
+
+            //find all contacts
+            var options = new ContactFindOptions();
+            options.filter = finder;
+            var fields = ["displayName", "name", "phoneNumbers"];
+            navigator.contacts.find(fields, onSuccess, onError, options);
+         }
+      }
+   },
+   //End Contacts
+
+   // Update DOM on a Received Event
+   receivedEvent: function (id) {
+      var parentElement = document.getElementById(id);
+      var listeningElement = parentElement.querySelector('.listening');
+      var receivedElement = parentElement.querySelector('.received');
+
+      listeningElement.setAttribute('style', 'display:none;');
+      receivedElement.setAttribute('style', 'display:block;');
+
+      console.log('Received Event: ' + id);
+   }
+};
+
+app.initialize();
