f63f14bd51ca82e3c6bf087a196a476584aa7ba2
Erwin Schwartz <erwin.schwartz.etudiant@gmail.com>
2016-04-22 10:08:16 +0200

    nouvelle page video + merge app.js et script.js

--- makeit/www/js/script.js
+++ makeit/www/js/script.js
@@ -30,9 +30,12 @@ var app = {
 
       //Call function for profil picture
       app.profilPicture();
+
+
       $('#contact-btn').click(function () {
          app.findContacts();
       });
+
 
       //Call Geolocation feature
       $('#geolocation-btn').click(function () {
@@ -295,3 +298,65 @@ var app = {
 };
 
 app.initialize();
+
+
+
+//ionic
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function ($ionicPlatform) {
+   $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+         cordova.plugins.Keyboard.disableScroll(true);
+
+      }
+      if (window.StatusBar) {
+         // org.apache.cordova.statusbar required
+         StatusBar.styleDefault();
+      }
+   });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+   $stateProvider
+
+      .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'AppCtrl'
+   })
+
+   .state('app.contact', {
+      url: '/contact',
+      views: {
+         'menuContent': {
+            templateUrl: 'templates/contact.html'
+         }
+      }
+   })
+
+   .state('app.video', {
+      url: '/video',
+      views: {
+         'menuContent': {
+            templateUrl: 'templates/video.html'
+         }
+      }
+   })
+
+   .state('app.home', {
+      url: '/home',
+      views: {
+         'menuContent': {
+            templateUrl: 'templates/home.html'
+         }
+      }
+   });
+
+   // if none of the above states are matched, use this as the fallback
+   $urlRouterProvider.otherwise('/app/home');
+});
